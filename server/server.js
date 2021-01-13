c930bac876c6ddc013f1934e661f71295dde11ec
Derek Shen <derekshen@Dereks-MBP-3.verizon.net>
2021-01-13 00:44:13 -0500

    added MongoDB team info

--- server/server.js
+++ server/server.js
@@ -33,9 +33,9 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "FILL ME IN";
+const mongoConnectionURL = "mongodb+srv://shiyyum:Tenniswiz123@kale-grater.arvck.mongodb.net/kale-grater?retryWrites=true&w=majority";
 // TODO change database name to the name you chose
-const databaseName = "FILL ME IN";
+const databaseName = "kale-grater";
 
 // connect to mongodb
 mongoose
