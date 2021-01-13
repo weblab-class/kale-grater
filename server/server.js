753a9c2c5d15f0b2dfffd557d84c0c4414da2be8
Derek Shen <derekshen@Dereks-MBP-3.verizon.net>
2021-01-13 01:08:18 -0500

    no mongodb password ;)

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "mongodb+srv://shiyyum:Tenniswiz123@kale-grater.arvck.mongodb.net/kale-grater?retryWrites=true&w=majority";
+const mongoConnectionURL = "mongodb+srv://shiyyum:<password>@kale-grater.arvck.mongodb.net/kale-grater?retryWrites=true&w=majority";
 // TODO change database name to the name you chose
 const databaseName = "kale-grater";
 
