14ea93065ebdbfebb737062cd986ebc4189e2560
Kailey Yang <kaileyy@mit.edu>
2021-01-16 17:12:28 -0500

    added structure to homepage

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, {Component} from "react";
 import GoogleLogin, { GoogleLogout } from "react-google-login";
 
 import "../../utilities.css";
-import "./LoginPage.css";
+import "./HomePage.css";
 
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        
+    }
+}
+export default HomePage;
