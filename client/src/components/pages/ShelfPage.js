a78b935f0114efb0ca5af5511e09ffb313813ba0
Kailey Yang <kaileyy@mit.edu>
2021-01-16 23:52:26 -0500

    initial structure

---
+++ client/src/components/pages/ShelfPage.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import GoogleLogin, { GoogleLogout } from "react-google-login";
+
+import NavBar from "../modules/NavBar.js";
+
+import "../../utilities.css";
+import "./ShelfPage.css";
+
+class ShelfPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+
+    };
+};
+export default ShelfPage;
