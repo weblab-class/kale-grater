de43acdb3ce8863b995c635da6a3c0c97d8ad137
Derek Shen <derekshen@Dereks-MBP-3.verizon.net>
2021-01-18 00:09:40 -0500

    single orb v1

--- client/src/components/modules/SingleOrb.js
+++ client/src/components/modules/SingleOrb.js
@@ -15,19 +15,35 @@ import "./SingleOrb.css";
  */
 
  class SingleOrb extends Component {
-     constructor(props) {
-         super(props);
-     }
+    constructor(props) {
+        super(props);
+        this.state = {
+          showContent: false,
+        }
+    }
+    
+    handleClick = () => {
+      this.setState({
+        showContent: true,
+      });
+    }
 
     render () {
         return (
-            <div>
-              <OrbContent
-                creator_id={this.props.creator_id}
-                emotion={this.props.emotion}
-                content={this.props.content}
-                timestamp = {this.props.timestamp}
-              />
+            <div onClick={this.handleClick}>
+              {this.state.showContent
+                ? <OrbContent
+                  creator_id={this.props.creator_id}
+                  emotion={this.props.emotion}
+                  content={this.props.content}
+                  timestamp = {this.props.timestamp}
+                  />
+                : <section className="SingleOrb-stage">
+                    <figure className="SingleOrb-ball">
+                      <span className="SingleOrb-shadow"></span>
+                    </figure>
+                  </section>
+              }
             </div>
           );
     }
