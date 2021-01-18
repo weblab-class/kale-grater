6c451b590b1bd0244dfca68461a1002c626d966f
Kailey Yang <kaileyy@mit.edu>
2021-01-17 23:04:53 -0500

    stuff

--- client/src/components/pages/ShelfPage.js
+++ client/src/components/pages/ShelfPage.js
@@ -23,33 +23,6 @@ class ShelfPage extends Component {
     }
 
     render() {
-<<<<<<< HEAD
-        return (
-            <section>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-4 col-md-2"><img src="http://placehold.it/150x190" class="img-responsive book"/></div>
-                        <div class="col-xs-4 col-md-2"><img src="http://placehold.it/150x190" class="img-responsive book"/></div>
-                        <div class="col-xs-4 col-md-2"><img src="http://placehold.it/150x190" class="img-responsive book"/></div>
-                        <div class="col-xs-12 shelf hidden-md hidden-lg"></div>
-                        <div class="col-xs-4 col-md-2"><img src="http://placehold.it/150x190" class="img-responsive book"/></div>
-                        <div class="col-xs-4 col-md-2"><img src="http://placehold.it/150x190" class="img-responsive book"/></div>
-                        <div class="col-xs-4 col-md-2"><img src="http://placehold.it/150x190" class="img-responsive book"/></div>
-                        <div class="col-xs-12 shelf"></div>
-                        <div class="col-xs-4 col-md-2"><img src="http://placehold.it/150x190" class="img-responsive book"/></div>
-                        <div class="col-xs-4 col-md-2"><img src="http://placehold.it/150x190" class="img-responsive book"/></div>
-                        <div class="col-xs-4 col-md-2"><img src="http://placehold.it/150x190" class="img-responsive book"/></div>
-                        <div class="col-xs-12 shelf hidden-md hidden-lg"></div>
-                        <div class="col-xs-4 col-md-2"><img src="http://placehold.it/150x190" class="img-responsive book"/></div>
-                        <div class="col-xs-4 col-md-2"><img src="http://placehold.it/150x190" class="img-responsive book"/></div>
-                        <div class="col-xs-4 col-md-2"><img src="http://placehold.it/150x190" class="img-responsive book"/></div>
-                        <div class="col-xs-12 shelf"></div>
-                    </div>
-                </div> 
-            </section>
-        );
-    };
-=======
         // return (
         //     <>
         //         <div>SHELF</div>
@@ -62,7 +35,6 @@ class ShelfPage extends Component {
             </div>
         )
     }
->>>>>>> ad325b7c1f7f44484ae9b9afc1a80d1be64f10de
 }
 
 export default ShelfPage;
