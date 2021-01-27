b7db98864c9045dafcd90f8f28963cecef4dd442
Kailey Yang <kaileyy@mit.edu>
2021-01-26 20:50:42 -0500

    attempt at handleclick

--- client/src/components/pages/ShelfPage.js
+++ client/src/components/pages/ShelfPage.js
@@ -19,8 +19,8 @@ class ShelfPage extends Component {
             userId: this.props.userId,
             userName: "",
             shelfView: "week",
-            // currentWeekOrbs: null,
-            // currentWeekStart: prevMonday
+            currentWeekOrbs: null,
+            currentWeekStart: null
         };
     }
 
@@ -99,9 +99,95 @@ class ShelfPage extends Component {
     // }
     
     handleClick = (change) => {
-        // this.setState({
-        //     currentWeekStart: this.state.currentWeekStart
-        // })
+        var prevMonday = new Date()
+        prevMonday.setDate(prevMonday.getDate() - (prevMonday.getDay() + 6) % 7);
+        var currentStart = this.state.currentWeekStart ? this.state.currentWeekStart : prevMonday
+        // console.log('B5', prevSunday)
+        // prevSunday.setDate(prevSunday.getDate() - (prevSunday.getDay() + 6) % 7);
+        // console.log('AFTER', prevSunday)
+        // console.
+        // var currentStart = 
+
+
+        
+        var month;
+        if (currentStart.getMonth() + 1 < 10) {
+          month = "0" + (currentStart.getMonth() + 1)
+        } else {
+          month = currentStart.getMonth() + 1
+        }
+        const prevMondayDate = currentStart.getFullYear() + "-" + month + "-" + currentStart.getDate();
+        // console.log('OIDSJFA', prevSundayDate)
+        // console.log('bruh', Date(prevSundayDate))
+        // var weekOrbs = {}
+        var allDayOrbs = []
+        var weekOrbs = []
+        const weekDayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+        
+        // for (let i = 0; i < 7; i++) {
+        //     // var current_result = new Date(prevSundayDate);
+        //     var current_result = new Date(prevSunday.getFullYear(), month - 1, prevSunday.getDate())
+        //     current_result.setDate(current_result.getDate() + i);
+        //     var resultMonth;
+        //     if (current_result.getMonth() + 1 < 10) {
+        //         resultMonth = "0" + (current_result.getMonth() + 1)
+        //     } else {
+        //         resultMonth = result.getMonth() + 1
+        //     }
+          
+        //     const resultDate = current_result.getFullYear() + "-" + resultMonth + "-" + current_result.getDate()
+        //     // console.log(resultDate)
+        //     function filterDay(orb) {
+        //         return orb.timestamp.startsWith(resultDate)
+        //     }
+
+        //     const properResultDate = resultMonth + "-" + current_result.getDate() + "-" + current_result.getFullYear();
+            
+        //     const dayOrbs = this.state.orbs.filter(filterDay)
+        //     var dayEmotions = ""
+        //     var dayEmotionsList = []
+        //     var orbClass = "MultiOrb-"
+        //     var contentList = []
+        //     for (let i = 0; i < dayOrbs.length; i++) {
+        //         const currentOrb = dayOrbs[i]
+        //         const currentEmotion = currentOrb.emotion
+        //         // if (!dayEmotions.startsWith(currentEmotion)) {
+        //         if (!dayEmotionsList.includes(currentEmotion)) {
+        //             dayEmotions = dayEmotions + currentEmotion
+        //             // numEmotions += 1
+                    
+        //         }
+        //         contentList = [currentOrb.content].concat(contentList)
+        //         dayEmotionsList = [currentEmotion].concat(dayEmotionsList)
+        //     }
+        //     if (dayEmotionsList.length === 0) {
+        //         orbClass += "clear";
+        //     } else if (dayEmotionsList.length > 2) {
+        //         orbClass += "mixed";
+        //     } else {
+        //         orbClass += dayEmotions;
+        //     }
+            
+        //     // console.log(numEmotions)
+        //     // console.log('ORBCLASS', orbClass)
+            
+        //     // weekOrbs = 
+        //     weekOrbs = [<MultiOrb className={`MultiOrb-orb ${orbClass}`}
+        //     // creator_id={.creator_id} 
+        //     // emotion={orbObj.emotion}
+        //     // content={orbObj.content}
+        //     // timestamp={orbObj.timestamp}
+        //     // privacy={orbObj.privacy}
+        //     // view={this.state.view}
+        //     emotions={dayEmotionsList}
+        //     content={contentList}
+        //     date={properResultDate}
+        //     day={weekDayNames[i]}
+        //     view={this.state.view}
+        //     />].concat(weekOrbs)
+        //     // allDayOrbs = [dayOrbs].concat(allDayOrbs)
+        //     // weekOrbs[i] = dayOrbs
+        // }
     }
     render() {
         if (!this.props.username) {
