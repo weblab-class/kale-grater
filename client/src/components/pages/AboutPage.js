fff784d5f83b5de61deb918122c810b7d292b185
graceyesong <73430159+graceyesong@users.noreply.github.com>
2021-01-26 19:09:36 -0500

    positioning

--- client/src/components/pages/AboutPage.js
+++ client/src/components/pages/AboutPage.js
@@ -39,22 +39,15 @@ class AboutPage extends Component {
         return (
             <>
             <div className="AboutPage-body">
-            <div className="AboutPage-orbContainer">
-                <figure className="AboutPage-anger AboutPage-Orb AboutPage-shadow"></figure>
-                <figure className="AboutPage-fear AboutPage-Orb AboutPage-shadow"></figure>
-                <figure className="AboutPage-joy AboutPage-Orb AboutPage-shadow"></figure>
-                <figure className="AboutPage-disgust AboutPage-Orb AboutPage-shadow"></figure>
-                <figure className="AboutPage-sadness AboutPage-Orb AboutPage-shadow"></figure>
-            </div>
 
             <div className="AboutPage-text">
                 <h2 className="AboutPage-header">How we all started</h2>
-                <div className="AboutPage-description">
-                    <p>On the eve of our Milestone 1 submission, our team member Derek Shen, frantic to get something submittable, closed his
+                <div>
+                    <div className="AboutPage-description">On the eve of our Milestone 1 submission, our team member Derek Shen, frantic to get something submittable, closed his
                         eyes and pointed to one of the bullet-pointed ideas on our Google Document. And thus our website was born...
-                    </p>
+                    </div>
 
-                    <p>
+                    <div className="AboutPage-description">
                     <strong>Just kidding! </strong> We (Kailey, Grace, Derek) made Memorble with the hopes of providing users a 
                     unique and effective way of visualizing their emotions. Inspired by the Pixar film 
                     <em><a href="https://www.imdb.com/title/tt2096673/"> Inside Out</a></em>, each memory entry is enclosed in a vibrant, glowing, 
@@ -62,15 +55,23 @@ class AboutPage extends Component {
                     experiencing and the remembering self: one can use the site to look back on the most defining moments of their lives, or, on another day,
                     they can zoom out into our long-term view to see the shapes and turns over the days, weeks, and years. 
                     
-                    </p>
+                    </div>
+                </div>
+
+                <div className="AboutPage-orbContainer">
+                <figure className="AboutPage-anger AboutPage-Orb AboutPage-shadow"></figure>
+                <figure className="AboutPage-fear AboutPage-Orb AboutPage-shadow"></figure>
+                <figure className="AboutPage-joy AboutPage-Orb AboutPage-shadow"></figure>
+                <figure className="AboutPage-disgust AboutPage-Orb AboutPage-shadow"></figure>
+                <figure className="AboutPage-sadness AboutPage-Orb AboutPage-shadow"></figure>
                 </div>
 
                 <h2 className="AboutPage-header">How to use this site / FAQs</h2>
-                <div className="AboutPage-description">
-                    <p>For users who haven't added anything, it might look bare or confusing. Talk about adding a memory and the different features,
-                    how many memories you can add, different views on the shelf, Bing Bong feature, social feature </p>
+                <div>
+                    <div className="AboutPage-description">For users who haven't added anything, it might look bare or confusing. Talk about adding a memory and the different features,
+                    how many memories you can add, different views on the shelf, Bing Bong feature, social feature </div>
 
-                    <p>
+                    <div className="AboutPage-description">
                     Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa 
                     quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas 
                     sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro 
@@ -78,7 +79,7 @@ class AboutPage extends Component {
                     ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit 
                     laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil 
                     molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur
-                    </p>
+                    </div>
                 </div>
 
                 <h2 className="AboutPage-header">Contact us!</h2>
